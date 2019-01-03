39f43b8956d0bf6274c88c40a35b9e728e08aad7
Jo du Plessis <jo@joduplessis.com>
2019-01-03 18:00:28 +0200

    Travis updates

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
-set -e
 
-LOCALPATH='./dist'
-REMOTEPATH='/public_html/'
-
-lftp -f "
-open ftp://$FTP_HOST
-user $FTP_USER $FTP_PASSWORD
-mirror --continue --reverse --delete $LOCALPATH $REMOTEPATH
-bye
-"
+lftp -u $FTP_USER,$FTP_PASSWORD $FTP_HOST -e "mirror -R -p ./dist/ public_html ; quit"
