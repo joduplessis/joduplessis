db652b35d4b50c72dc6d5cfc7bf58925db4d3a49
Jo du Plessis <jo@joduplessis.com>
2020-10-25 22:13:50 +0100

    Content updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,7 +20,7 @@
             <a href="https://www.npmjs.com/~joduplessis" target="_blank"><img src="../assets/icons/npmjs-fill.svg" /></a>
   					<a href="https://www.linkedin.com/in/jo-duplessis/" target="_blank"><img src="../assets/icons/linkedin-box-fill.svg" /></a>
   					<a href="https://twitter.com/jo_duplessis" target="_blank"><img src="../assets/icons/twitter-fill.svg" /></a>
-						<a href="https://joduplessis.com/resume" target="_blank"><img src="../assets/icons/file-text-fill.svg" /></a>
+						<!-- <a href="https://joduplessis.com/resume" target="_blank"><img src="../assets/icons/file-text-fill.svg" /></a> -->
   				</div>
   			</div>
   		</div>
