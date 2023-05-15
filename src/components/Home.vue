6a074e66a83b1f19ffb538e4c6dd371e45fca375
Jo du Plessis <jo@joduplessis.com>
2023-05-15 13:55:41 +0200

    Wording updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,7 @@
 					<div class="links">
 						<a href="https://github.com/joduplessis" target="_blank">GitHub</a>
 						<a href="https://www.linkedin.com/in/jo-duplessis/" target="_blank">LinkedIn</a>
-						<a href="https://twitter.com/jo_duplessis" target="_blank">Twitter</a>
+						<!-- <a href="https://twitter.com/jo_duplessis" target="_blank">Twitter</a> -->
 						<!--
 						<router-link :to="{ name: 'Work'}">Archives</router-link>
 						<a href="https://fold.dev" target="_blank">
